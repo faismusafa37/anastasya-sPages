602b631002ac488ed0e5ca73c4a47ae6afcedc18
faismusafa37 <faismusafa37@gmail.com>
2024-05-26 05:19:35 +0700

    push

--- js/main.js
+++ js/main.js
@@ -18,6 +18,24 @@
         $(window).resize(toggleNavbarMethod);
     });
     
+    document.addEventListener('DOMContentLoaded', function () {
+        // Get the current file name
+        var path = window.location.pathname;
+        var page = path.split("/").pop();
+
+        // Get all nav links
+        var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+        // Loop through the nav links and add the active class to the current page link
+        navLinks.forEach(function (link) {
+            var linkPage = link.getAttribute('href');
+            if (linkPage === page) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    });
     
     // Skills
     $('.skills').waypoint(function () {
